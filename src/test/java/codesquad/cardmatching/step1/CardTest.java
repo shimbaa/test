4f9339eacacd717e1c437b96e52f7a77b9c7e3f0
shimbaa <shim9597@gmail.com>
2023-12-02 17:53:44 +0900

    test: 카드타입에 따른 동일성 테스트

---
+++ src/test/java/codesquad/cardmatching/step1/CardTest.java
@@ -0,0 +1,27 @@
+package codesquad.cardmatching.step1;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CardTest {
+
+    @DisplayName("같은 타입의 카드는 동일한 카드로 취급되어야 한다")
+    @Test
+    void sameCardTypeEquals() {
+        Card card1 = Card.from(CardType.ONE);
+        Card card2 = Card.from(CardType.ONE);
+
+        assertThat(card1).isEqualTo(card2);
+    }
+
+    @DisplayName("다른 타입의 카드는 다른 카드로 취급되어야 한다")
+    @Test
+    void differentCardTypeNotEquals() {
+        Card card1 = Card.from(CardType.ONE);
+        Card card2 = Card.from(CardType.TWO);
+
+        assertThat(card1).isNotEqualTo(card2);
+    }
+}
